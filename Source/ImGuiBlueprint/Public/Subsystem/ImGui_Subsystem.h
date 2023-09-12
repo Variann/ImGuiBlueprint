8a11e3900c72056d62ceb3488f9844e8069a1db7
Variann <kjartandth@gmail.com>
2023-09-12 07:23:40 +0200

    Added ImGui subsystem

---
+++ Source/ImGuiBlueprint/Public/Subsystem/ImGui_Subsystem.h
@@ -0,0 +1,31 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "O_ImGuiDebugger.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "ImGui_Subsystem.generated.h"
+
+
+UCLASS()
+class IMGUIBLUEPRINT_API UImGui_Subsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY()
+	TArray<UO_ImGuiDebugger*> Debuggers;
+
+	/**Adds a debugger and enable its tick event.
+	 * Returns true if it was successful.*/
+	UFUNCTION(Category="ImGui Subsystem", BlueprintCallable)
+	bool AddDebugger(UO_ImGuiDebugger* Debugger);
+
+	UFUNCTION(Category="ImGui Subsystem", BlueprintCallable)
+	bool RemoveDebugger(UO_ImGuiDebugger* Debugger);
+
+	UFUNCTION(Category="ImGui Subsystem", BlueprintCallable)
+	bool RemoveDebuggerByIdentifier(FName Identifier);
+};
